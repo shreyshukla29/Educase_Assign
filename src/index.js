d2d55faeb44886bc245757ebabdae7028d44e59c
shrey <shreyshukla101217@gmail.com>
2024-08-27 18:16:15 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ const dotenv = require("dotenv");
 const connection = require("./config/db_config");
 const router = require("./routers/school.routes");
 dotenv.config();
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 // Middleware to parse JSON
 app.use(express.json());
 app.use(express.urlencoded());
